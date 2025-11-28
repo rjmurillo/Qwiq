00fab15962135437a8a0dc3edd64fa19853321cf
Richard Murillo <rjmurillo@gmail.com>
2025-11-28 12:55:26 -0800

    chore: remove JetBrains.Annotations dependency
    This change removes the embedded ReSharper.Annotations.cs file and all usage
    of JetBrains.Annotations attributes throughout the codebase. Modern C# and
    .NET provide nullable reference types and code analysis attributes that
    serve similar purposes.
    
    Changes:
    - Delete src/ReSharper.Annotations.cs (1272 lines)
    - Remove [NotNull], [CanBeNull], [ItemNotNull], [ItemCanBeNull] attributes
    - Remove [Pure], [InstantHandle], [UsedImplicitly] attributes
    - Remove [ContractAnnotation], [StringFormatMethod] attributes
    - Remove [PublicAPI], [MustUseReturnValue] attributes
    - Remove Compile Include references from 5 csproj files
    - Delete GlobalSuppressions.cs files with only JetBrains suppressions
    
    Note: JetBrains.dotMemoryUnit in integration tests is retained (different package).

--- src/Qwiq.Core.Rest/IdentityDescriptor.cs
+++ src/Qwiq.Core.Rest/IdentityDescriptor.cs
@@ -1,14 +1,1 @@
-using System;
-
-namespace Qwiq.Client.Rest
-{
-    public class IdentityDescriptor : Qwiq.IdentityDescriptor
-    {
-        internal IdentityDescriptor(Microsoft.VisualStudio.Services.Identity.IdentityDescriptor descriptor)
-            : base(
-                (descriptor ?? throw new ArgumentNullException(nameof(descriptor))).IdentityType,
-                descriptor.Identifier)
-        {
-        }
-    }
-}
+using System;namespace Qwiq.Client.Rest{    public class IdentityDescriptor : Qwiq.IdentityDescriptor    {        internal IdentityDescriptor(Microsoft.VisualStudio.Services.Identity.IdentityDescriptor descriptor)            : base(                (descriptor ?? throw new ArgumentNullException(nameof(descriptor))).IdentityType,                descriptor.Identifier)        {        }    }}
--- src/Qwiq.Core.Soap/QueryDefinition.cs
+++ src/Qwiq.Core.Soap/QueryDefinition.cs
@@ -1,16 +1,1 @@
-using System;
-
-namespace Qwiq.Client.Soap
-{
-    internal class QueryDefinition : Qwiq.QueryDefinition
-    {
-        internal QueryDefinition(Microsoft.TeamFoundation.WorkItemTracking.Client.QueryDefinition queryDefinition)
-            : base(
-                (queryDefinition ?? throw new ArgumentNullException(nameof(queryDefinition))).Id,
-                queryDefinition.Name,
-                queryDefinition.QueryText,
-                queryDefinition.Path)
-        {
-        }
-    }
-}
+using System;namespace Qwiq.Client.Soap{    internal class QueryDefinition : Qwiq.QueryDefinition    {        internal QueryDefinition(Microsoft.TeamFoundation.WorkItemTracking.Client.QueryDefinition queryDefinition)            : base(                (queryDefinition ?? throw new ArgumentNullException(nameof(queryDefinition))).Id,                queryDefinition.Name,                queryDefinition.QueryText,                queryDefinition.Path)        {        }    }}
