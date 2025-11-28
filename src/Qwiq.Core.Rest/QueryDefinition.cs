260bd6b9c121d2597614bf88f094074859b4baf1
Richard Murillo <rjmurillo@gmail.com>
2025-11-28 12:55:26 -0800

    chore: remove JetBrains.Annotations dependency
    This change removes the embedded ReSharper.Annotations.cs file and all usage
    of JetBrains.Annotations attributes throughout the codebase. Modern C# and
    .NET provide nullable reference types and code analysis attributes that
    serve similar purposes.
    
    Changes:
    - Delete src/ReSharper.Annotations.cs (1272 lines)
    - Remove [NotNull], [CanBeNull], [ItemNotNull], [ItemCanBeNull] attributes
    - Remove [Pure], [InstantHandle], [UsedImplicitly] attributes
    - Remove [ContractAnnotation], [StringFormatMethod] attributes
    - Remove [PublicAPI], [MustUseReturnValue] attributes
    - Remove Compile Include references from 5 csproj files
    - Delete GlobalSuppressions.cs files with only JetBrains suppressions
    
    Note: JetBrains.dotMemoryUnit in integration tests is retained (different package).

--- src/Qwiq.Core.Rest/QueryDefinition.cs
+++ src/Qwiq.Core.Rest/QueryDefinition.cs
@@ -1,17 +1,1 @@
-using System;
-using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
-
-namespace Qwiq.Client.Rest
-{
-    internal class QueryDefinition : Qwiq.QueryDefinition
-    {
-        internal QueryDefinition(QueryHierarchyItem queryDefinition)
-            : base(
-                (queryDefinition ?? throw new ArgumentNullException(nameof(queryDefinition))).Id,
-                queryDefinition.Name,
-                queryDefinition.Wiql,
-                queryDefinition.Path)
-        {
-        }
-    }
-}
+using System;using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;namespace Qwiq.Client.Rest{    internal class QueryDefinition : Qwiq.QueryDefinition    {        internal QueryDefinition(QueryHierarchyItem queryDefinition)            : base(                (queryDefinition ?? throw new ArgumentNullException(nameof(queryDefinition))).Id,                queryDefinition.Name,                queryDefinition.Wiql,                queryDefinition.Path)        {        }    }}
