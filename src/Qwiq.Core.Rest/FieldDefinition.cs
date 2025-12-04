0854b25a39026bed872454a9afd53ac74b5d2bf1
Richard Murillo <rjmurillo@gmail.com>
2025-12-03 19:25:55 -0800

    fix(rest,soap): Fix null checks before base constructor calls

--- src/Qwiq.Core.Rest/FieldDefinition.cs
+++ src/Qwiq.Core.Rest/FieldDefinition.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Diagnostics.Contracts;
-
 
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 
@@ -9,11 +7,10 @@ namespace Qwiq.Client.Rest
     internal class FieldDefinition : Qwiq.FieldDefinition
     {
         internal FieldDefinition(WorkItemFieldReference field)
-            : base(field.ReferenceName, field.Name)
+            : base(
+                (field ?? throw new ArgumentNullException(nameof(field))).ReferenceName,
+                field.Name)
         {
-            Contract.Requires(field != null);
-
-            if (field == null) throw new ArgumentNullException(nameof(field));
         }
     }
 }
