9c9d85dd8a47bea1628cacaf885aae0975abbb0b
Richard Murillo <rjmurillo@gmail.com>
2025-12-05 15:11:03 -0800

    refactor(core): rewrite polyfills using C# 14 extension blocks
    - Use extension(ArgumentNullException) syntax from SimonCropp/Polyfill
    - Separate CallerArgumentExpressionAttribute to its own file
    - Set LangVersion=preview for C# 14 support

--- src/Qwiq.Core/Compatibility/ArgumentNullExceptionPolyfill.cs
+++ src/Qwiq.Core/Compatibility/ArgumentNullExceptionPolyfill.cs
@@ -1,41 +1,28 @@
 // Polyfill for ArgumentNullException.ThrowIfNull for .NET Framework and .NET Standard 2.0
-//
-// This file provides a compatibility shim for ArgumentNullException.ThrowIfNull which is only available
-// in .NET 6+. For net472 and netstandard2.0 targets, we provide our own implementation.
+// Based on: https://github.com/SimonCropp/Polyfill
+// We cannot use the Polyfill NuGet package directly due to conflicts with VSS Client polyfills.
 
-#if NETFRAMEWORK || NETSTANDARD2_0
+#pragma warning disable
 
-// ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+static partial class Polyfill
 {
-    // Polyfill CallerArgumentExpression attribute
-    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    extension(ArgumentNullException)
     {
-        public CallerArgumentExpressionAttribute(string parameterName)
-        {
-            ParameterName = parameterName;
-        }
-
-        public string ParameterName { get; }
-    }
-}
-
-namespace System
-{
-    internal static partial class ArgumentNullException
-    {
-        /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
-        /// <param name="argument">The reference type argument to validate as non-null.</param>
-        /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
-        public static void ThrowIfNull(object? argument, [Runtime.CompilerServices.CallerArgumentExpression("argument")] string? paramName = null)
+#if !NET6_0_OR_GREATER
+        // Link: https://learn.microsoft.com/en-us/dotnet/api/system.argumentnullexception.throwifnull
+        public static void ThrowIfNull(
+            [NotNull] object? argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
         {
             if (argument is null)
             {
                 throw new ArgumentNullException(paramName);
             }
         }
+#endif
     }
 }
-
-#endif
