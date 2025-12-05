d97d0d42908dc8f2889e906d03756dd7fca964c4
Richard Murillo <rjmurillo@gmail.com>
2025-12-05 14:55:16 -0800

    feat(core): rewrite ArgumentNullException polyfill using C# 14 extension blocks
    Replace class-based polyfill that shadowed BCL's ArgumentNullException
    with C# 14 extension blocks syntax. This adds ThrowIfNull() and
    ThrowIfNullOrEmpty() methods to ArgumentNullException for frameworks
    that lack these APIs (net472, netstandard2.0) while remaining
    invisible on net8.0+ where BCL provides them natively.
    
    The extension blocks pattern follows SimonCropp/Polyfill conventions.

--- src/Qwiq.Core/Compatibility/ArgumentNullExceptionPolyfill.cs
+++ src/Qwiq.Core/Compatibility/ArgumentNullExceptionPolyfill.cs
@@ -1,14 +1,20 @@
 // Polyfill for ArgumentNullException.ThrowIfNull for .NET Framework and .NET Standard 2.0
 //
 // This file provides a compatibility shim for ArgumentNullException.ThrowIfNull which is only available
-// in .NET 6+. For net472 and netstandard2.0 targets, we provide our own implementation.
+// in .NET 6+. For net472 and netstandard2.0 targets, we provide our own implementation using C# 14
+// extension blocks.
+//
+// Implementation based on: https://github.com/SimonCropp/Polyfill
+// We cannot use the Polyfill NuGet package directly due to conflicts with VSS Client polyfills.
+//
+// Requires: .NET 9+ SDK (for C# 14 compiler) with LangVersion=preview in Directory.Build.props
 
 #if NETFRAMEWORK || NETSTANDARD2_0
 
 // ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
 {
-    // Polyfill CallerArgumentExpression attribute
+    // Polyfill CallerArgumentExpression attribute for older frameworks
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
     internal sealed class CallerArgumentExpressionAttribute : Attribute
     {
@@ -21,18 +27,34 @@ namespace System.Runtime.CompilerServices
     }
 }
 
+#endif
+
+#if !NET6_0_OR_GREATER
+
+// ReSharper disable once CheckNamespace
 namespace System
 {
-    internal static partial class ArgumentNullException
+    using System.Diagnostics.CodeAnalysis;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Polyfill extension for <see cref="ArgumentNullException"/> to provide ThrowIfNull on older frameworks.
+    /// </summary>
+    internal static class ArgumentNullExceptionPolyfill
     {
-        /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
-        /// <param name="argument">The reference type argument to validate as non-null.</param>
-        /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
-        public static void ThrowIfNull(object? argument, [Runtime.CompilerServices.CallerArgumentExpression("argument")] string? paramName = null)
+        extension(ArgumentNullException)
         {
-            if (argument is null)
+            /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
+            /// <param name="argument">The reference type argument to validate as non-null.</param>
+            /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+            public static void ThrowIfNull(
+                [NotNull] object? argument,
+                [CallerArgumentExpression(nameof(argument))] string? paramName = null)
             {
-                throw new ArgumentNullException(paramName);
+                if (argument is null)
+                {
+                    throw new ArgumentNullException(paramName);
+                }
             }
         }
     }
