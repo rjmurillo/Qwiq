329ede1fbe55672c73b3c3853a91844d95644e4e
Richard Murillo <rjmurillo@gmail.com>
2025-12-08 14:54:11 -0800

    refactor(tests): convert Core.Tests REST tests to integration tests
    - Replace ignored WireMock unit tests with real integration tests
    - Remove WireMock.Net mocking code (now in separate WireMock test classes)
    - Tests now connect to qwiq-sandbox.visualstudio.com for validation
    - Use Windows authentication with interactive prompts
    - Update to use TestData constants for work item IDs
    
    This completes the separation of unit tests (WireMock-based, fast, offline)
    from integration tests (real Azure DevOps, slower, requires connectivity).

--- test/Qwiq.Core.Tests/Client/Rest/RestWorkItemStoreQueryTests.cs
+++ test/Qwiq.Core.Tests/Client/Rest/RestWorkItemStoreQueryTests.cs
@@ -1,163 +1,128 @@
-using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using Qwiq.Client.Rest;
-using Qwiq.Credentials;
 using Qwiq.Tests.Common;
 using Shouldly;
 using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Threading.Tasks;
-using WireMock.RequestBuilders;
-using WireMock.ResponseBuilders;
-using WireMock.Server;
 
 namespace Qwiq.Client.Rest
 {
     /// <summary>
-    /// Tests for REST client query execution using WireMock.Net for HTTP mocking.
+    /// Integration tests for REST client query execution against qwiq-sandbox.
     /// </summary>
     /// <remarks>
-    /// These tests demonstrate the refactored factory pattern that enables unit testing without Azure DevOps connectivity.
-    /// The WorkItemStoreFactory now accepts an internal ITfsConnectionFactory parameter for test injection.
-    /// 
-    /// KNOWN ISSUE: VssConnection attempts to establish a connection during construction, requiring SSL certificate validation.
-    /// Future enhancement: Configure HttpClient to accept WireMock's self-signed certificate or use a different mocking approach.
+    /// These tests validate the REST client's ability to execute WIQL queries against
+    /// a real Azure DevOps instance. They use Windows authentication with interactive
+    /// login prompts when needed.
+    ///
+    /// Test Environment:
+    /// - Organization: https://qwiq-sandbox.visualstudio.com
+    /// - Project: WIT
+    /// - See TestData.cs for work item IDs and test user information.
     /// </remarks>
     [TestClass]
-    [TestCategory("RestUnit")]
-    [Ignore("SSL certificate validation pending - see class remarks")]
-    public class Given_WorkItemStore_When_Querying_With_WIQL : ContextSpecification
+    [TestCategory("REST")]
+    public class Given_WorkItemStore_When_Querying_With_WIQL : TimedContextSpecification
     {
-        private WireMockServer? _server;
         private IWorkItemStore? _store;
         private IWorkItemCollection? _result;
-        private const string TestWiql = "SELECT [System.Id] FROM WorkItems WHERE [System.WorkItemType] = 'Bug'";
+
+        // Query for the basic test Bug work item
+        private static readonly string TestWiql = ((FormattableString)$"SELECT [System.Id], [System.WorkItemType], [System.Title] FROM WorkItems WHERE [System.Id] = {TestData.BasicWorkItemId}").ToString(CultureInfo.InvariantCulture);
 
         public override void Given()
         {
-            // Start WireMock server with HTTPS support
-            var settings = new WireMock.Settings.WireMockServerSettings
-            {
-                UseSSL = true,
-                Port = 0 // Use random port
-            };
-            _server = WireMockServer.Start(settings);
-
-            // Configure mock response for WIQL query
-            _server
-                .Given(Request.Create()
-                    .WithPath("/*/_apis/wit/wiql")
-                    .UsingPost())
-                .RespondWith(Response.Create()
-                    .WithStatusCode(200)
-                    .WithHeader("Content-Type", "application/json")
-                    .WithBody(@"{
-                        ""queryType"": ""flat"",
-                        ""queryResultType"": ""workItem"",
-                        ""asOf"": ""2025-12-06T00:00:00.000Z"",
-                        ""workItems"": [
-                            {
-                                ""id"": 1,
-                                ""url"": """ + _server.Url + @"/_apis/wit/workItems/1""
-                            },
-                            {
-                                ""id"": 2,
-                                ""url"": """ + _server.Url + @"/_apis/wit/workItems/2""
-                            }
-                        ]
-                    }"));
-
-            // Mock the work items endpoint
-            _server
-                .Given(Request.Create()
-                    .WithPath("/*/_apis/wit/workItems")
-                    .UsingGet())
-                .RespondWith(Response.Create()
-                    .WithStatusCode(200)
-                    .WithHeader("Content-Type", "application/json")
-                    .WithBody(@"{
-                        ""count"": 2,
-                        ""value"": [
-                            {
-                                ""id"": 1,
-                                ""rev"": 1,
-                                ""fields"": {
-                                    ""System.Id"": 1,
-                                    ""System.WorkItemType"": ""Bug"",
-                                    ""System.Title"": ""Test Bug 1"",
-                                    ""System.State"": ""Active"",
-                                    ""System.TeamProject"": ""TestProject""
-                                },
-                                ""url"": """ + _server.Url + @"/_apis/wit/workItems/1""
-                            },
-                            {
-                                ""id"": 2,
-                                ""rev"": 1,
-                                ""fields"": {
-                                    ""System.Id"": 2,
-                                    ""System.WorkItemType"": ""Bug"",
-                                    ""System.Title"": ""Test Bug 2"",
-                                    ""System.State"": ""Active"",
-                                    ""System.TeamProject"": ""TestProject""
-                                },
-                                ""url"": """ + _server.Url + @"/_apis/wit/workItems/2""
-                            }
-                        ]
-                    }"));
-
-            // Create mock connection factory that points to WireMock server
-            var mockConnectionFactory = new MockTfsConnectionFactory(new Uri(_server.Url!));
-
-            // Create authentication options with a simple credentials factory
-            // Credentials don't matter for WireMock, just need to provide something
-            var options = new AuthenticationOptions(
-                new Uri(_server.Url!),
-                AuthenticationTypes.Basic,
-                _ => new VssCredentials[] { new VssBasicCredential(string.Empty, string.Empty) });
-
-            // Get the factory instance and use internal overload for testing
-            var factory = (WorkItemStoreFactory)WorkItemStoreFactory.Default;
-            _store = factory.Create(options, mockConnectionFactory);
+            // Create REST store using integration settings (Windows auth with interactive prompt)
+            _store = TimedAction(() => IntegrationSettings.CreateRestStore(), "REST", "Create WorkItemStore");
         }
 
         public override void When()
         {
-            // Execute query against WireMock server
-            _result = _store?.Query(TestWiql);
+            // Execute query against Azure DevOps
+            _result = TimedAction(() => _store?.Query(TestWiql), "REST", "Query WIQL");
         }
 
         [TestMethod]
-        public void Should_Return_Two_WorkItems()
+        public void Should_Return_One_WorkItem()
         {
             _result.ShouldNotBeNull();
-#pragma warning disable CA1829, CA1826 // Use Count property when available
-            _result.Count().ShouldBe(2);
+#pragma warning disable CA1829, CA1826 // Use Count property when available - IWorkItemCollection doesn't have Count
+            _result.Count().ShouldBe(1);
 #pragma warning restore CA1829, CA1826
         }
 
         [TestMethod]
-        public void Should_Have_Correct_WorkItem_Ids()
+        public void Should_Have_Correct_WorkItem_Id()
         {
             _result.ShouldNotBeNull();
-            var ids = _result.Select(wi => wi.Id).ToArray();
-            ids.ShouldContain(1);
-            ids.ShouldContain(2);
+            var workItem = _result.First();
+            workItem.Id.ShouldBe(TestData.BasicWorkItemId);
         }
 
         [TestMethod]
-        public void Should_Have_Correct_WorkItem_Types()
+        public void Should_Have_Bug_WorkItem_Type()
         {
             _result.ShouldNotBeNull();
-            _result.All(wi => wi.Type?.Name == "Bug").ShouldBeTrue();
+            var workItem = _result.First();
+            workItem.Type?.Name.ShouldBe("Bug");
         }
 
         public override void Cleanup()
         {
-            _server?.Stop();
-            _server?.Dispose();
             (_store as IDisposable)?.Dispose();
+            base.Cleanup();
+        }
+    }
+
+    /// <summary>
+    /// Integration tests for REST client querying multiple work items.
+    /// </summary>
+    [TestClass]
+    [TestCategory("REST")]
+    public class Given_WorkItemStore_When_Querying_Multiple_Bugs : TimedContextSpecification
+    {
+        private IWorkItemStore? _store;
+        private IWorkItemCollection? _result;
+
+        // Query for all Bug work items in the project
+        private const string TestWiql = "SELECT [System.Id], [System.WorkItemType], [System.Title] FROM WorkItems WHERE [System.WorkItemType] = 'Bug' AND [System.TeamProject] = 'WIT'";
+
+        public override void Given()
+        {
+            _store = TimedAction(() => IntegrationSettings.CreateRestStore(), "REST", "Create WorkItemStore");
+        }
+
+        public override void When()
+        {
+            _result = TimedAction(() => _store?.Query(TestWiql), "REST", "Query Multiple Bugs");
+        }
+
+        [TestMethod]
+        public void Should_Return_At_Least_One_WorkItem()
+        {
+            _result.ShouldNotBeNull();
+            _result.Any().ShouldBeTrue("Expected at least one Bug work item in the WIT project");
+        }
+
+        [TestMethod]
+        public void Should_All_Be_Bug_Type()
+        {
+            _result.ShouldNotBeNull();
+            _result.All(wi => wi.Type?.Name == "Bug").ShouldBeTrue("All returned work items should be Bugs");
+        }
+
+        [TestMethod]
+        public void Should_Include_Known_Test_Bug()
+        {
+            _result.ShouldNotBeNull();
+            var ids = _result.Select(wi => wi.Id).ToArray();
+            ids.ShouldContain(TestData.BasicWorkItemId, $"Expected to find Bug ID {TestData.BasicWorkItemId}");
+        }
+
+        public override void Cleanup()
+        {
+            (_store as IDisposable)?.Dispose();
+            base.Cleanup();
         }
     }
 }
